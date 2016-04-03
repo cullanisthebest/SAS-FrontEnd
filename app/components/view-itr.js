a6db22edd213282193321327685883771b99ca31
Cullan Liang <cliang29@uwo.ca>
2016-04-03 19:37:04 -0400

    front too nice

--- app/components/view-itr.js
+++ app/components/view-itr.js
@@ -15,13 +15,21 @@ export default Ember.Component.extend({
 			var selectedStudent = myStore.findRecord('student', this.get('ID')).then(function(student){
 				self.set('firstname', student.get('firstName'));
 			});
-
+			myStore.findAll('itrprogram');
 			myStore.query('itrprogram', {student: this.get('ID')}).then(function (itrprograms) {
 				console.log(itrprograms);
-				self.set('itrprograms', itrprograms);
+				console.log(itrprograms.objectAt(0)!=undefined);
+				if(itrprograms.objectAt(0)!=undefined){
+					var orderedArray= new Array(10);
+					for(var i=0;i<10;i++)
+					{
+						orderedArray[itrprograms.objectAt(i).get('order')-1]=itrprograms.objectAt(i);
+					}
+					self.set('itrprograms', orderedArray);
+				}
+			}).then(function(){
+				self.set('isItrFormEditing', true);
 			});
-
-			this.set('isItrFormEditing', true);
 		},
 
 		cancel () {
